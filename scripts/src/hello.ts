6892cf42120a4f11fe90272aeb1f1b947ef2eee2
vinimarcondes2004-oss <vinimarcondes2004@gmail.com>
2026-03-20 16:21:34 -0300

    criei servidor

--- scripts/src/hello.ts
+++ scripts/src/hello.ts
@@ -1,1 +1,11 @@
-console.log("Hello from @workspace/scripts");
+import http from "http"
+
+const PORT = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.end("Funcionando 🚀")
+})
+
+server.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT)
+})
